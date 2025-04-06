42fa5733370257ad1e2d8b0c2392c714366c4d9a
Pablo Souza <contato@souzapablo.dev.br>
2025-04-06 10:36:42 -0300

    feat: add basic functionality to `Entity`

--- src/FinanceTracker.Api/Shared/Entity.cs
+++ src/FinanceTracker.Api/Shared/Entity.cs
@@ -3,7 +3,16 @@
 public abstract class Entity
 {
     public long Id { get; private set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
-    public bool IsDeleted { get; set; } = false;
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public DateTime LastUpdate { get; private set; } = DateTime.UtcNow;
+    public bool IsDeleted { get; private set; } = false;
+
+    protected void Update() =>
+        LastUpdate = DateTime.UtcNow;
+
+    public void Delete()
+    {
+        LastUpdate = DateTime.UtcNow;
+        IsDeleted = true;
+    }
 }
---
+++ test/FinanceTracker.UnitTests/Entities/EntityTests.cs
@@ -0,0 +1,32 @@
+﻿using FinanceTracker.Api.Entities;
+
+namespace FinanceTracker.UnitTests.Entities;
+public class EntityTests
+{
+    [Fact]
+    public void ShouldSetIsDeletedToTrue_WhenDeleted()
+    {
+        // Arrange
+        var sut = new Account("Test Account");
+
+        // Act
+        sut.Delete();
+        
+        // Assert
+        Assert.True(sut.IsDeleted);
+    }
+
+    [Fact]
+    public void ShouldChangeLastUpdate_WhenUpdated()
+    {
+        // Arrange
+        var sut = new Account("Test Account");
+        var initialDate = sut.LastUpdate;
+
+        // Act
+        sut.Delete();
+
+        // Assert
+        Assert.NotEqual(initialDate, sut.LastUpdate);
+    }
+}
